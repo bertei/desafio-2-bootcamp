5764e97d63e05e4845a5bbdb59ed7f49e17e1ca3
bernardo <bteisceira@gmail.com>
2022-04-08 15:56:48 +0000

    ADD: automation.sh script & dependencies

--- automation.sh
+++ automation.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-echo "hello world"
+echo "-------------"
+echo "Creating /tempdir and subdirectories /tempdir/templates & /tempdir/static"
+sudo mkdir /tempdir
+sudo mkdir /tempdir/templates
+sudo mkdir /tempdir/static
+echo "Directory & subdirectories created"
+echo "-------------"
+echo "Copying files into /tempdir/templates & /tempdir/static..."
+sudo cp -r ./static/. /tempdir/templates/
+sudo cp -r ./templates/. /tempdir/static/
+sudo cp ./desafio2_app.py /tempdir/
+echo "Files copied to mentioned directories.."
+echo "-------------"
+echo "Creating DockerFile..."
+sudo touch /tempdir/Dockerfile
+sudo chmod 666 /tempdir/Dockerfile
+sudo cat << EOF >> /tempdir/Dockerfile
+FROM python
+RUN pip install flask
+COPY ./static /home/myapp/static
+COPY ./templates /home/myapp/templates
+COPY desafio2_app.oy /home/myapp/
+EXPOSE 5050
+CMD python3 /home/myapp/desafio2_app.py
+EOF
